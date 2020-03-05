baa3ef2ced9e033fbe4fe6fdfd8aa552d20a4375
Stephen Hamilton <stephen.hamilton@usma.edu>
2020-03-04 21:10:23 -0500

    working render

--- iq-transform-vtk.py
+++ iq-transform-vtk.py
@@ -30,35 +30,117 @@ def get_files():
     #outfile.write('i, q, t, c\n') # inphase, quadrature, time, and color (angle of i and q?)
     return iqdata, outfile
 
-def build_vtkfile(iqdata, outfile):
+def get_polydata(iqdata, outfile):
     rawiq = np.fromfile(iqdata, dtype='f', count = -1)
     point_count = int(rawiq.size/2)
     iqtwo = rawiq.reshape(point_count, 2)
-    iqtwo = iqtwo*100 #scale the IQ data
+    iqtwo = iqtwo*100000 #scale the IQ data
     #z = np.arange(point_count)
-    z = np.arange(0,10000,10000/point_count)  
+    z = np.arange(0,100000,100000/point_count)  
     iqthree = np.column_stack((iqtwo,z))
-    print(str(rawiq[0]), str(rawiq[1])) #sanity check
-    vtkdata = numpy_support.numpy_to_vtk(iqthree, deep=True, array_type=vtk.VTK_FLOAT)
+    #print(str(rawiq[0]), str(rawiq[1])) #sanity check
+
+    #plot just I data in 2d and extend the array
+    qaxis = np.zeros(point_count)
+    i2data = np.column_stack((qaxis,iqtwo[:,0] ))
+    idata = np.column_stack((i2data, z))
+    iq_with_iaxis = np.append(iqthree,idata)
+
+    vtkdata = numpy_support.numpy_to_vtk(iq_with_iaxis, deep=False, array_type=vtk.VTK_FLOAT)
     vtkdata.SetNumberOfComponents(3)
     vtkdata.SetName("Points")
+
+
+    #idatavtk = numpy_support.numpy_to_vtk(idata, deep=False, array_type=vtk.VTK_FLOAT)
+    #idatavtk.SetNumberOfComponents(3)
+    #idatavtk.SetName("Idata")
     #table = vtk.vtkTable
     #table.addColumn(vtkdata)
+    #lines = vtk.vtkCellArray()
+    #for p in range(point_count-1):
+    #    line = vtk.vtkLine()
+    #    line.GetPointIds().SetId(0,p)
+    #    line.GetPointIds().SetId(0,p+1)
+    #    lines.InsertNextCell(line)
     points = vtk.vtkPoints()
     points.SetData(vtkdata)
     pd = vtk.vtkPolyData()
     pd.SetPoints(points)
-    vv = vtk.vtkPointDataToCellData()
-    vv.SetInputData(pd)
-    vv.Update()
-    
-    
+    pd.GetPointData().AddArray(vtkdata)
+    #pd.GetPointData().AddArray(idatavtk)
+    vg = vtk.vtkVertexGlyphFilter()
+    vg.SetInputData(pd)
+    vg.Update()
+    poly = vg.GetOutput()
+    #poly.SetLines(lines)
     w = vtk.vtkXMLPolyDataWriter()
-    w.SetInputData(vv.GetOutput())
+    w.SetInputData(poly)
     w.SetFileName("autotest.vtp")
     w.Write()
-    pdb.set_trace()
+    #pdb.set_trace()
 
+def viewdata():
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInputData(normals.GetOutput())
+    mapper.ScalarVisibilityOn()
+    mapper.SetScalarRange(-1,1)
+    mapper.SetScalarModeToUsePointFieldData()
+    mapper.ColorByArrayComponent("Velocity", 0)
+    #print image
+    #print contour
+
+    #mapper.SelectColorArray("Q-criterion")
+    #mapper.SetLookupTable(lut)
+
+    print mapper
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
+    
+    ren = vtk.vtkRenderer()
+    ren.AddActor(actor)
+    ren.SetBackground(1,1,1)
+    ren.ResetCamera()
+
+    renWin = vtk.vtkRenderWindow()
+    renWin.SetSize(600,600)
+    renWin.AddRenderer(ren)
+    iren = vtk.vtkRenderWindowInteractor()
+    def MouseMove(self, data):
+        print("Load Cache %s" % data )
+        print ("Iren data")
+        #print iren
+        #addcube
+        #print ren
+        print ren.GetViewPoint()
+        print ren.GetDisplayPoint()
+        print ren.WorldToView()
+        print ren.ComputeVisiblePropBounds()
+        ysize = renWin.GetSize()[1]
+        c.SetValue(0,ysize)
+        c.Update()
+        normals = vtk.vtkPolyDataNormals()
+        normals.SetInputData(c.GetOutput())
+        normals.SetFeatureAngle(25) #?
+        normals.Update()
+
+        normals.SetFeatureAngle(45) #?
+        normals.Update()
+        mapper2 = vtk.vtkPolyDataMapper()
+        mapper2.SetInputData(normals.GetOutput())
+        mapper2.ScalarVisibilityOn()
+        mapper2.SetScalarRange(-.5,1)
+        mapper2.SetScalarModeToUsePointFieldData()
+        mapper2.ColorByArrayComponent("Velocity", 0)
+        actor2 = vtk.vtkActor()
+        actor2.SetMapper(mapper2)
+        ren.AddActor(actor2)
+
+ 
+    iren.AddObserver("MiddleButtonPressEvent", MouseMove)
+
+    iren.SetRenderWindow(renWin)
+    iren.Initialize()
+    iren.Start()
 
 def main():
     iqdata, outfile = get_files()
